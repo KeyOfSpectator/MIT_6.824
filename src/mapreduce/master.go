3b340e2325437352a6fe9b8a88befea55c5ad2c1
冯诗淳 <413070456@qq.com>
2016-03-01 15:30:19 +0800

    try to finish master

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -28,5 +28,75 @@ func (mr *MapReduce) KillWorkers() *list.List {
 
 func (mr *MapReduce) RunMaster() *list.List {
   // Your code here
+
+  //
+  l := list.New()
+
+  // map
+  cur_miss := make(chan int)
+  //var ch [len(mr.Workers)] chan int // mutex channels
+  cur_miss <- 0
+  for wi, w := range mr.Workers {
+    //ch[wi] = make(chan int)
+    go func(){
+      var cur_map := <- cur_miss
+      for cur_map < mr.nMap { //while
+
+        go func(){
+          // call
+          args := &DoJobArgs{ mr.file , &JobType{ Map } , cur_map , mr.nMap}
+          var reply DoJobReply;
+          ok := call(w.address, "Worker.DoJob", args, &reply)
+          if ok == false {
+            fmt.Printf("DoWork: RPC %s DoJob error\n", w.address)
+          } else {
+           l.PushBack(cur_map)
+          }
+          cur_miss <- (cur_map+1)
+        }()
+
+        cur_map := <- cur_miss
+        
+      }
+    }()
+
+  }
+
+  // reduce
+  cur_miss := make(chan int)
+  //var ch [len(mr.Workers)] chan int // mutex channels
+  cur_miss <- 0
+  for wi, w := range mr.Workers {
+    //ch[wi] = make(chan int)
+    go func(){
+      var cur_red := <- cur_miss
+      for cur_red < mr.nReduce { //while
+
+        go func(){
+          // call
+          args := &DoJobArgs{ mr.file , &JobType{ Reduce } , cur_red , mr.nReduce}
+          var reply DoJobReply;
+          ok := call(w.address, "Worker.DoJob", args, &reply)
+          if ok == false {
+            fmt.Printf("DoWork: RPC %s DoJob error\n", w.address)
+          } else {
+           l.PushBack(cur_red)
+          }
+          cur_miss <- (cur_red+1)
+        }()
+        
+        cur_red := <- cur_miss
+        
+      }
+    }()
+
+  }
+  
+
+  // reduce
+  for i := 0; i < mr.nReduce; i++ {
+    DoReduce(i, mr.file, mr.nMap, Reduce)
+  }
+
   return mr.KillWorkers()
 }
